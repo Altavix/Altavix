2b8259995e4364f0aa49022fe99e50e461026639
Artem <admrozdail@gmail.com>
2026-09-03 22:07:40 +0300

    Added logging.

--- AltavixAPI/Program.cs
+++ AltavixAPI/Program.cs
@@ -8,98 +8,121 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-builder.Configuration.AddJsonFile("secrets.json", optional: true, reloadOnChange: true);
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
 
-builder.Services.AddApplivation();
-builder.Services.AddPersistence(builder.Configuration);
-builder.Services.AddControllers();
-
-builder.Services.AddCors(options =>
+try
 {
-    options.AddPolicy("AllowFrontend", policy =>
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, services, configuration) => configuration.ReadFrom.Configuration(context.Configuration).ReadFrom.Services(services));
+    builder.Configuration.AddJsonFile("secrets.json", optional: true, reloadOnChange: true);
+
+    builder.Services.AddApplivation();
+    builder.Services.AddPersistence(builder.Configuration);
+    builder.Services.AddControllers();
+
+    builder.Services.AddCors(options =>
     {
-        policy.WithOrigins("http://localhost:5173", "https://localhost:5173")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+        options.AddPolicy("AllowFrontend", policy =>
+        {
+            policy.WithOrigins("http://localhost:5173", "https://localhost:5173")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
     });
-});
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    
+    
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-// Configure Identity
-builder.Services.AddIdentityCore<UserEntity>()
-    .AddRoles<RoleEntity>()
-    .AddEntityFrameworkStores<AltavixDbContext>();
+    // Configure Identity
+    builder.Services.AddIdentityCore<UserEntity>()
+        .AddRoles<RoleEntity>()
+        .AddEntityFrameworkStores<AltavixDbContext>();
 
-// Configure JWT Authentication
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
+    // Configure JWT Authentication
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
         {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
-            ValidAudience = builder.Configuration["JwtSettings:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Secret"]!))
-        };
-
-        options.Events = new JwtBearerEvents
-        {
-            OnMessageReceived = context =>
+            options.TokenValidationParameters = new TokenValidationParameters
             {
-                if (context.Request.Cookies.ContainsKey("accessToken"))
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Secret"]!))
+            };
+
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
                 {
-                    context.Token = context.Request.Cookies["accessToken"];
+                    if (context.Request.Cookies.ContainsKey("accessToken"))
+                    {
+                        context.Token = context.Request.Cookies["accessToken"];
+                    }
+
+                    return Task.CompletedTask;
                 }
-                return Task.CompletedTask;
-            }
-        };
-    });
-builder.Services.AddAuthorization();
+            };
+        });
+    builder.Services.AddAuthorization();
 
 
-builder.Services.AddAutoMapper(config =>
-{
-    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
-    config.AddProfile(new AssemblyMappingProfile(typeof(IAltavixDbContext).Assembly));
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(config =>
+    builder.Services.AddAutoMapper(config =>
     {
-        config.RoutePrefix = string.Empty;
-        config.SwaggerEndpoint("swagger/v1/swagger.json", "Altavix API V1");
-        config.OAuthClientId("altavix-api-swagger");
-        config.OAuthScopes("AltavixWebApi");
-        config.OAuthUsePkce();
-
+        config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+        config.AddProfile(new AssemblyMappingProfile(typeof(IAltavixDbContext).Assembly));
     });
+
+    var app = builder.Build();
+    app.UseSerilogRequestLogging(); 
+    
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(config =>
+        {
+            config.RoutePrefix = string.Empty;
+            config.SwaggerEndpoint("swagger/v1/swagger.json", "Altavix API V1");
+            config.OAuthClientId("altavix-api-swagger");
+            config.OAuthScopes("AltavixWebApi");
+            config.OAuthUsePkce();
+
+        });
+    }
+
+    app.UseHttpsRedirection();
+    app.UseCors("AllowFrontend");
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<AltavixDbContext>();
+        DbInitializer.Initialize(context);
+
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<RoleEntity>>();
+        await RolesInitializer.InitializeAsync(roleManager);
+    }
+
+    app.MapControllers();
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-app.UseCors("AllowFrontend");
-app.UseAuthentication();
-app.UseAuthorization();
-
-using (var scope = app.Services.CreateScope())
+catch (Exception ex)
 {
-    var context = scope.ServiceProvider.GetRequiredService<AltavixDbContext>();
-    DbInitializer.Initialize(context);
-
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<RoleEntity>>();
-    await RolesInitializer.InitializeAsync(roleManager);
+    Log.Fatal(ex, "Application terminated unexpectedly");
 }
-app.MapControllers();
-app.Run();
+finally
+{
+    Log.CloseAndFlush();
+}
